d36e38bf13688983f8c2262f37ce287a08896ecf
Kris <kristinaovchinnikova22@gmail.com>
2024-12-21 13:02:08 +0600

    I made add new comment, get all comment, get comment by news id and delete comment by id

--- api/index.ts
+++ api/index.ts
@@ -3,20 +3,30 @@ import cors from "cors";
 import fs = require("fs");
 import newsRouter from "./routes/news";
 import newsDb from "./newsDb";
+import commentRouter from "./routes/comments";
+import commentsDb from "./commentsDb";
 
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 app.use('/news', newsRouter);
+app.use('/comments', commentRouter)
 
 const run = async () => {
     if (fs.existsSync('./news.json')) {
         await newsDb.init()
     } else {
+        fs.writeFileSync('./comments.json', JSON.stringify([]));
+    }
+    if (fs.existsSync('./comments.json')) {
+        await commentsDb.init()
+    } else {
         fs.writeFileSync('./news.json', JSON.stringify([]));
     }
+
 
     app.listen(port, () => {
         console.log(`Server started on port http://localhost:${port}`);
--- api/routes/comments.ts
+++ api/routes/comments.ts
@@ -1,10 +1,59 @@
 import express from "express";
+import newsDb from "../newsDb";
+import {IComment, ICommentMutation} from "../types";
+import commentsDb from "../commentsDb";
 
 const commentRouter = express.Router();
 
 commentRouter.post('/', async (req, res) => {
-    if(!req.body.newsId || req.body.comment){
+    if(!req.body.newsId || !req.body.comment){
         res.status(404).send({'error': ' Comment and NewsId must be filled out'})
+    }
+    const newsIdByCheck = await newsDb.newsById(req.body.newsId);
+
+    if(newsIdByCheck){
+        let newComment: ICommentMutation ={
+            newsId: req.body.newsId,
+            author: req.body.author ? req.body.author : 'Anonymous',
+            comment: req.body.comment,
+        }
+        newComment = await  commentsDb.addComments(newComment);
+        res.send(newComment);
+    }else{
+        res.status(404).send({'error': ' NewsId not found'})
+    }
+
+    if(!newsIdByCheck){
+        res.status(404).send({'error': ' News not found'})
     }
 
 })
+
+commentRouter.get("/", async (req, res) => {
+    let comments : IComment[] = [];
+    if(req.query.newsId){
+        const comment = await  commentsDb.commentByNewsId(String(req.query.newsId));
+        if(comment){
+            res.send(comment)
+        }else{
+            res.status(40).send({'error': ' Comment not found'})
+        }
+    }else{
+        comments = await commentsDb.getComments()
+        res.send(comments)
+    }
+})
+
+commentRouter.delete("/:id", async (req, res) => {
+    if(!req.params.id){
+        res.status(400).send({'error':' ID params must be in url'});
+    }
+    const comment = await  commentsDb.deleteComment(req.params.id);
+    if(comment){
+        res.send(comment)
+    }else{
+        res.status(404).send({'error':' Comment not found'})
+    }
+})
+
+export default commentRouter
